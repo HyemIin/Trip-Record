1cf4124550c66cce71a10a53f731fb47b93bd094
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-11-10 10:52:50 +0900

    initial commit

---
+++ src/main/java/com/ybe/ybe_toyproject3/YbeToyProject3Application.java
@@ -0,0 +1,13 @@
+package com.ybe.ybe_toyproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbeToyProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YbeToyProject3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ybe/ybe_toyproject3/YbeToyProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybe.ybe_toyproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbeToyProject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
