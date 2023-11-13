a75046f6a3bc660bbeb3315b4d7f30d2a1868a26
Su-daa <subin.kk019@gmail.com>
2023-11-13 17:28:32 +0900

    Feat: LikesRepository 구현

--- src/main/java/com/ybe/ybe_toyproject3/domain/likes/repository/LikesRepository.java
+++ src/main/java/com/ybe/ybe_toyproject3/domain/likes/repository/LikesRepository.java
@@ -3,6 +3,9 @@ package com.ybe.ybe_toyproject3.domain.likes.repository;
 import com.ybe.ybe_toyproject3.domain.likes.model.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 
+    Optional<Likes> findByTripIdAndUserId(Long tripId, Long userId);
 }
