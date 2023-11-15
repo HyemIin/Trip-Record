c4c9db89758d00c3e1a09db4aa4d39e83a21a861
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-11-15 10:34:08 +0900

    feat: user-service 테스트 코드 생성

---
+++ src/test/java/integrate/com/ybe/ybe_toyproject3/domain/user/service/AuthServiceIntegrateTest.java
@@ -0,0 +1,17 @@
+package com.ybe.ybe_toyproject3.domain.user.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceIntegrateTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/integrate/com/ybe/ybe_toyproject3/domain/user/service/UserServiceIntegrateTest.java
@@ -0,0 +1,17 @@
+package com.ybe.ybe_toyproject3.domain.user.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceIntegrateTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
