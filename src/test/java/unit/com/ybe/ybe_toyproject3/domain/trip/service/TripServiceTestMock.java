93f05a7d4c38b5ad000df2c6fe4b22976506ef38
chadongmin <cdm2883@naver.com>
2023-11-16 15:58:59 +0900

    Test : 여행수정, 삭제 Service 테스트 추가

--- src/test/java/unit/com/ybe/ybe_toyproject3/domain/trip/service/TripServiceTestMock.java
+++ src/test/java/unit/com/ybe/ybe_toyproject3/domain/trip/service/TripServiceTestMock.java
@@ -2,8 +2,10 @@ package com.ybe.ybe_toyproject3.domain.trip.service;
 
 import com.ybe.ybe_toyproject3.domain.itinerary.repository.ItineraryRepository;
 import com.ybe.ybe_toyproject3.domain.trip.dto.request.TripCreateRequest;
+import com.ybe.ybe_toyproject3.domain.trip.dto.request.TripUpdateRequest;
 import com.ybe.ybe_toyproject3.domain.trip.dto.response.TripCreateResponse;
 import com.ybe.ybe_toyproject3.domain.trip.dto.response.TripListResponse;
+import com.ybe.ybe_toyproject3.domain.trip.dto.response.TripUpdateResponse;
 import com.ybe.ybe_toyproject3.domain.trip.model.Trip;
 import com.ybe.ybe_toyproject3.domain.trip.repository.TripRepository;
 import com.ybe.ybe_toyproject3.domain.user.model.User;
@@ -21,9 +23,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
 
 import static com.ybe.ybe_toyproject3.global.common.type.TripType.DOMESTIC;
+import static com.ybe.ybe_toyproject3.global.common.type.TripType.GLOBAL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -89,7 +92,7 @@ public class TripServiceTestMock {
     }
 
     @Test
-    @DisplayName("여행조회 테스트")
+    @DisplayName("여행조회 테스트 - 검색기능")
     public void findAllTripTest() throws Exception {
         TripCreateRequest tripCreateRequest1 = TripCreateRequest.builder()
                 .tripName("일본")
@@ -130,16 +133,117 @@ public class TripServiceTestMock {
         when(tripRepository.findAll()).thenReturn(tripList);
         when(tripRepository.findAllByTripNameContaining(SEARCH_CONDITION))
                 .thenReturn(tripList.stream()
-                        .filter(t -> t.getTripName().startsWith(SEARCH_CONDITION)||
+                        .filter(t -> t.getTripName().startsWith(SEARCH_CONDITION) ||
                                 t.getTripName().endsWith(SEARCH_CONDITION))
                         .toList());
 
+        //검색 조건이 없는 경우
         List<TripListResponse> tripListResponses = tripService.findAllTrips(null);
-        List<TripListResponse> tripListResponsesWithSearchCondition = tripService.findAllTrips(SEARCH_CONDITION);
-
         assertEquals(2, tripListResponses.size());
+
+        //검색 조건이 있는 경우
+        List<TripListResponse> tripListResponsesWithSearchCondition = tripService.findAllTrips(SEARCH_CONDITION);
         assertEquals(1, tripListResponsesWithSearchCondition.size());
-        System.out.println(tripListResponsesWithSearchCondition.stream().map(TripListResponse::getTripName));
-//
+    }
+
+    @Test
+    @DisplayName("여행 수정 테스트")
+    public void tripEditTest() throws Exception {
+        TripCreateRequest tripCreateRequest1 = TripCreateRequest.builder()
+                .tripName("일본")
+                .tripStartDate(LocalDateTime.now().minusDays(10))
+                .tripEndDate(LocalDateTime.now().minusDays(1))
+                .tripType(DOMESTIC)
+                .build();
+        TripCreateRequest tripCreateRequest2 = TripCreateRequest.builder()
+                .tripName("중국")
+                .tripStartDate(LocalDateTime.now().minusDays(10))
+                .tripEndDate(LocalDateTime.now().minusDays(1))
+                .tripType(DOMESTIC)
+                .build();
+        Trip trip = tripCreateRequest1.toEntity();
+
+        Trip trip2 = tripCreateRequest2.toEntity();
+        User user = User.builder()
+                .id(1L)
+                .name("cha")
+                .email("cdm2883@naver.com")
+                .password("1234")
+                .authority(Authority.ROLE_USER)
+                .build();
+
+        trip.addUser(user);
+        trip2.addUser(user);
+        trip.updateTripId(1L);
+
+        List<Trip> tripList = List.of(trip, trip2);
+
+
+        List<TripListResponse> tripListResponseList = new ArrayList<>();
+
+        for (var t : tripList) {
+            TripListResponse tripListResponse = TripListResponse.fromEntity(t);
+            tripListResponseList.add(tripListResponse);
+        }
+        TripUpdateRequest tripUpdateRequest = TripUpdateRequest.builder()
+                .tripName("미국")
+                .tripStartDate(LocalDateTime.now().minusDays(10))
+                .tripEndDate(LocalDateTime.now().minusDays(1))
+                .tripType(GLOBAL)
+                .build();
+
+
+        assertEquals(2, tripListResponseList.size());
+        given(tripRepository.countTripByIdAndTripName(1L,tripUpdateRequest.getTripName()))
+                .willReturn(1);
+        given(tripRepository.findById(1L))
+                .willReturn(Optional.of(trip));
+        given(securityUtil.getCurrentUserId()).willReturn(1L);
+
+        TripUpdateResponse tripUpdateResponse = tripService.editTripById(1L, tripUpdateRequest);
+        //verify(tripRepository.countTripByIdAndTripName(1L, tripUpdateResponse.getTripName()),times(1));
+        verify(tripRepository, times(1)).countTripByIdAndTripName(eq(1L), eq(tripUpdateResponse.getTripName()));
+
+    }
+
+    @Test
+    @DisplayName("여행삭제 테스트")
+    public void deleteTripTest() {
+        //given
+        TripCreateRequest tripCreateRequest = TripCreateRequest.builder()
+                .tripName("여행1")
+                .tripStartDate(LocalDateTime.now().minusDays(10))
+                .tripEndDate(LocalDateTime.now().minusDays(1))
+                .tripType(DOMESTIC)
+                .build();
+        Trip trip = tripCreateRequest.toEntity();
+
+        given(securityUtil.getCurrentUserId()).willReturn(1L);
+        long userId = securityUtil.getCurrentUserId();
+        assertEquals(1L, userId);
+
+        given(userRepository.getUserById(userId)).willReturn(
+                User.builder()
+                        .id(1L)
+                        .name("cha")
+                        .password("1234")
+                        .email("cdm2883@naver.com")
+                        .authority(Authority.ROLE_USER)
+                        .build()
+        );
+        User createdUser = userRepository.getUserById(1L);
+        assertEquals("cha", createdUser.getName());
+        trip.addUser(createdUser);
+
+        //given
+        given(securityUtil.getCurrentUserId()).willReturn(1L);
+        given(tripRepository.findById(1L)).willReturn(Optional.of(trip));
+        //when
+        String deleteResultString = tripService.deleteTrip(1L);
+
+        verify(tripRepository).deleteById(1L);
+        assertEquals("1번 여행이 삭제되었습니다.", deleteResultString);
     }
 }
+
+
